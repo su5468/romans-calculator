67f86b8581b04c24f1e0ec078870f64074d9b248
su5468 <su5468@naver.com>
2024-01-31 23:25:38 +0900

    fix: 다시 절대경로로 전부 수정
    - 생각해보니 페이지에 따라 상대경로가 다르기도 하고, 같은 js 참조하는데 그러면 처리가 어려움.
    - 그냥 전부 절대경로로 수정함.
    - 주의 : 원래 로컬에서 할 때는 최상위 경로를 계산기 페이지로 가정해서 수정하기 전엔 라이브 페이지 돌리면 제대로 적용 안 될 것.

--- script.js
+++ script.js
@@ -14,16 +14,16 @@ $((e) => {
     // 헤더, 네비게이션바, 푸터 로드.
     // 헤더는 로드 후에 테마 설정 기능을 생성함.
     // 네비게이션바는 로드 후에 햄버거버튼의 토글 기능을 생성함.
-    $('#divHeaderContainer').load('./template.html header', (e) => {
+    $('#divHeaderContainer').load('romans-calculator/template.html header', (e) => {
         $('.button-color').on('click', (e) => {
             setThemeColor($(e.target).val());
         });
     });
-    $('#divNavContainer').load('./template.html nav.navigation-menu', (e) => {
+    $('#divNavContainer').load('romans-calculator/template.html nav.navigation-menu', (e) => {
         $('.hamburger-button').on('click', (e) => {
             $('.navigation-menu').toggleClass('active');
         });
     });
-    $('#divFooterContainer').load('./template.html footer');
+    $('#divFooterContainer').load('romans-calculator/template.html footer');
 
 });
